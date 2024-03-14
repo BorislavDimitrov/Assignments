aca31926ba92a7a0cf785475d24b18b1e0099dcf
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2024-03-14 14:33:22 +0200

    Complete Email send Assignment

---
+++ DisposalAndGarbageCollection/DisposalAndGarbageCollection/EmailSender.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace DisposalAndGarbageCollection
+{
+    public class EmailSender
+    {
+        private string smtpServer = "smtp.gmail.com";
+        private int port = 587;
+        private bool useSsl = true;
+
+        public async Task SendEmailAsync(string recipientEmail, string subject, string body, string gmailAddress, string appPassword)
+        {
+            using (var mailMessage = new MailMessage())
+            {
+                mailMessage.From = new MailAddress(gmailAddress);
+                mailMessage.To.Add(new MailAddress(recipientEmail));
+                mailMessage.Subject = subject;
+                mailMessage.Body = body;
+
+                using (var smtpClient = new SmtpClient(smtpServer, port))
+                {
+                    smtpClient.EnableSsl = useSsl;
+                    smtpClient.Credentials = new NetworkCredential(gmailAddress, appPassword);
+                    await smtpClient.SendMailAsync(mailMessage);
+                }
+            }
+        }
+    }
+}
---
+++ DisposalAndGarbageCollection/DisposalAndGarbageCollection/Program.cs
@@ -0,0 +1,27 @@
+﻿using DisposalAndGarbageCollection;
+
+try
+{ 
+    Console.WriteLine("Enter recipient email: ");
+    string recipientEmail = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(recipientEmail))
+    {
+        throw new ArgumentNullException();
+    }
+
+    //The email you send from
+    string gmailAddress = "testemailsendertestt@gmail.com\r\n";
+
+    //Password generate from the SMTP server used to send the emails
+    string appPassword = "kdjv vjpo jjpl kftp";
+
+    var emailSender = new EmailSender();
+    await emailSender.SendEmailAsync(recipientEmail, "Newsletter Subscription", "Thank you for subscribing to the newsletter.", gmailAddress, appPassword);
+
+    Console.WriteLine("Email sent successfully!");
+}
+catch
+{
+    Console.WriteLine("Something happend and the email was not sent successfully");
+}
