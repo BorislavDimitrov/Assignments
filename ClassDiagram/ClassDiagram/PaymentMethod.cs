67c54271a19e4a811a1b4f83442066f2139786c0
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2024-03-28 11:09:15 +0200

    Delete unnecesarry properties

--- ClassDiagram/ClassDiagram/PaymentMethod.cs
+++ ClassDiagram/ClassDiagram/PaymentMethod.cs
@@ -8,7 +8,6 @@
         public string CardType { get; set; } 
         public string CardNumber { get; private set; } 
         public string ExpirationDate { get; private set; } 
-        public bool IsDefault { get; set; }
         public List<Order> Orders { get; set; }
         public OrderStatus OrderStatus { get; set; }
     }
